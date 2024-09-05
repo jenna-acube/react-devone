2534d855d42ce21d0a288ff40b6215e9f4b1ecce
unknown <jenna@acubetechnologies.in>
2024-09-05 19:01:07 +0530

    test

---
+++ src/Card.jsx
@@ -0,0 +1,12 @@
+// import profilePic from './assets/imresizer-1725450169135.jpg'
+function Card(){
+    return(
+        <div className="card">
+            <img className="card-image" src="https://via.placeholder.com/200" alt="profile picture"></img>
+            <h2 className="card-title">Jenna Fathima</h2>
+            <p className="">I am a software engineer and do coding</p>
+        </div>
+    );
+}
+
+export default Card
---
+++ src/ColorPicker.jsx
@@ -0,0 +1,5 @@
+function ColorPicker()
+{
+    return(<></>);
+}
+export ColorPicker
